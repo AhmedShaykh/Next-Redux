7afa62aaf0629cae11e75ceed5848e8e42168dc4
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-11 17:32:39 +0500

    Add CounterSlice

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import CounterSlice from './slice/CounterSlice';
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        CounterSlice
+    }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
