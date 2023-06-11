f85cf109d407a4f832f2fc71609e215255f5a776
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-06-11 23:58:42 +0500

    Add Add & Reset Functionality

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React, { useState } from 'react';
 import { counterActions } from "./store/slice/CounterSlice";
 import { RootState } from "./store/Store";
 import { useSelector, useDispatch } from "react-redux";
@@ -11,12 +12,18 @@ const Counter = () => {
 
     const dispatch = useDispatch();
 
+    const [incrementAmount, setIncrementAmount] = useState<string>("2");
+
     const increment = () => {
         dispatch(counterActions.increment())
     };
 
     const decrement = () => {
         dispatch(counterActions.decrement())
+    };
+
+    const reset = () => {
+        dispatch(counterActions.reset())
     };
 
     return (
@@ -37,6 +44,29 @@ const Counter = () => {
                 onClick={decrement}
             >
                 Decrement
+            </button>
+
+            <div className="flex justify-between items-center gap-x-6">
+                <input
+                    type="text"
+                    className="w-14 text-center items-center text-black h-11 outline-none font-bold"
+                    value={incrementAmount}
+                    onChange={(e) => setIncrementAmount(e.target.value)}
+                />
+
+                <button
+                    className="p-3 rounded-md bg-blue-800 font-semibold text-white"
+                    onClick={() => dispatch(counterActions.incrementByAmount(Number(incrementAmount) || 0))}
+                >
+                    Add Amount
+                </button>
+            </div>
+
+            <button
+                className="p-3 rounded-md bg-white font-semibold text-black"
+                onClick={reset}
+            >
+                Reset
             </button>
         </div>
     )
