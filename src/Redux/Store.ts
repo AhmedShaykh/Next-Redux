2d8ea3f865928a12968c5791832b79aef4683ebd
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-24 20:33:56 +0500

    Delete Auth Slice

--- src/Redux/Store.ts
+++ src/Redux/Store.ts
@@ -1,10 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./features/authSlice";
 
 export const store = configureStore({
-    reducer: {
-        authReducer
-    }
+    reducer: {}
 });
 
 export type RootState = ReturnType<typeof store.getState>;
